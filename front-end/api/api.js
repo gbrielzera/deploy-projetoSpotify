eec1165e22f15b7097d430f0e5ebf29a35001e3e
Gabriel Cézar <151453514+gbrielzera@users.noreply.github.com>
2025-05-10 17:47:58 -0300

    Add files via upload

---
+++ front-end/api/api.js
@@ -0,0 +1,16 @@
+// Fetch ou Axios
+import "dotenv/config";
+import axios from "axios";
+
+// const { NODE_ENV } = process.env;
+// const URL = "http://localhost:3001/api";
+const {NODE_ENV} = process.env;
+const URL = NODE_ENV === 'development'? "https://deploy-jornada-full-stack.onrender.com/api" : "/api"
+
+const responseArtists = await axios.get(`${URL}/artists`);
+const responseSongs = await axios.get(`${URL}/songs`);
+
+export const artistArray = responseArtists.data;
+export const songsArray = responseSongs.data;
+
+// console.log(responseArtists.data);
